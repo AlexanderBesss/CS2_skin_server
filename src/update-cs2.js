9565e846822e8220d179649b9af27516063c739d
Oleksandr Besarab <alexandrbesss@gmail.com>
2025-11-22 10:39:49 +0200

    removed start script

--- src/update-cs2.js
+++ src/update-cs2.js
@@ -1,18 +1,37 @@
 import { runCommands } from './tools/run-script.js';
+import { spawn } from 'node:child_process';
+
+/**
+ * @param {String} process 
+ * @param {String[]} commands 
+ */
+export async function runCommands(processName, commands) {
+    const child = spawn(processName, commands);
+
+    for await (const chunk of child.stdout) {
+        console.log('[OUT]', chunk.toString());
+    }
+
+    for await (const chunk of child.stderr) {
+        console.log('[ERR]', chunk.toString());
+    }
+
+    console.log('Command exited with:', child.exitCode);
+}
 
 async function updateCS2() {
     const OS = process.platform;
     switch (OS) {
         case 'linux':
-            await runCommands([
+            await runCommands('docker', [
                 'compose',
                 '-f',
                 './steamcmd/linux/compose.yml',
                 'up'
-            ], 'docker');
+            ]);
             break;
         case 'win32':
-            await runCommands([
+            await runCommands('cmd', [
                 '/c',
                 './steamcmd/windows/update_cs2.bat'
             ]);
